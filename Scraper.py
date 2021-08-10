9a7f4d2d10da8f8686c15ec6a53122da71b3e626
Aaron Wright <aaronjohnwright@gmail.com>
2021-08-10 10:42:10 +0100

    first part done

--- Scraper.py
+++ Scraper.py
@@ -1,1 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
+
+# Submit a GET requet to the URL and retrieves HTML data that the server sends back
+response = requests.get("https://www.drupal.org/docs/develop/coding-standards/list-of-sql-reserved-words")
+
+# Create beautiful soup object with HTML parser
+content = response.content
+parser = BeautifulSoup(content, "html.parser")
+
+ordered_list = parser.find_all("ol")
+
+print(ordered_list)
+keywords = ordered_list[0].find_all("li")
+
+#for keyword in keywords:
+    #print(keyword.text)
+
